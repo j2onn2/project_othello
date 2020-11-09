0497bce21e9c660200093b9afa5f176c02e7b0c7
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-11-10 00:34:05 +0900

    .

--- main.c
+++ main.c
@@ -10,43 +10,71 @@ void	init_othello	( ) {
 	
 		int x , y	;
 		
-		for(x=0; x<N; x++){
-		for (y=0; y<N; y++)
-		{
-			board [x][y]= 0 ;
-		}
-		}
+		//행과 열 for문으로 초기조건 표현함 
+		for(x=0; x<N; x++)
+			{
+				for (y=0; y<N; y++)
+				{board [x][y]= 0 ;}	
+			}
+		
 		// 첫 실행에 정중앙 4칸에 흰색과 검정색 알 2개씩 놈 
-		board [N/2-1][N/2-1] = 'O'	;
-		board [N/2-1][N/2] = 'X'	;
-		board [N/2][N/2-1] = 'X'	;
-		board [N/2][N/2] = 'O'	;
-}
+		//print 함수에서 1을 O 2를 X로 쓸 것이므로 초기조건을 이렇게 지정해줌 
+		
+		board [N/2-1][N/2-1] = 1	;
+		board [N/2-1][N/2] = 2	;
+		board [N/2][N/2-1] = 2	;
+		board [N/2][N/2] = 1	;
+	}
+
+
 
 void	print_othello () {
 	
-	int i, 	j, k	;
+	int i, 	j, k, l	, m;
+	
+
+	printf("   ")	;
 	for (i=0; i<N; i++)
 	{
-		printf(" %d ", i)	;	
+		printf(" %d ", i)	;		//첫 줄의 0~5 표현 
 	}
 	
 	printf ("\n")	;
-	
-	for (j=0; j<N; j++)
+	printf ("   ")	;
+	  
+	for (j=0; j<N; j++)			//구분선 표현 
 	{
-		printf("---")	;	
+		printf("---")	;	 
 	}
 	
 	printf("\n")	;
 	
-	for (k=0 ; k<N ; k++)
+	for (k=0 ; k<N ; k++)		//세로줄 0~5까지 표현 
 	{
-		if 
-		printf("%d | %c | \n", k, )	;
+		printf("%d| ",k )	;	
 		
+		for(l=0 ; l<N ; l++)	//행 하나 당 나와있는 열 표현하기 위해 for문을 한번 더 씀 
+		{
+			
+			if (board[k][l] ==0)	//0일 때는 빈칸으로 놔둠 
+			{ printf("  |")	;	}
+			
+			else if (board[k][l] ==1)	// O일 때는 1로 나타낼 것임 
+			{ printf(" O|")	;	}
+			
+			else if (board[k][l]== 2)	// X일 때는 2로 나타낼 것임 
+			{ printf(" X|")	;	}
+			
+		}
+		printf("\n")	;
+		for (m=0; m<N+1; m++)	//구분선 표현 
+		{
+		printf("---")	;	 
+		}
+		
+		printf("\n")	;
 	}
-} 
+}
 
 
 void main () {
